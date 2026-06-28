f0b1aca050fcd4fd7cdb5b97071116044ba896e0
Bhavana Jain <bhavana53jain@gmail.com>
2026-06-29 01:41:16 +0530

    Add Login Page Object for Automation Exercise

--- tests/smoke/test_login.py
+++ tests/smoke/test_login.py
@@ -1,18 +1,75 @@
-from pages.home_page import HomePage
-from pages.login_page import LoginPage
+from pages.base_page import BasePage
 
 
-def test_open_login_page(page):
+class LoginPage(BasePage):
     """
-    Verify that the user can navigate
-    from the Home Page to the Login Page.
+    Page Object for Automation Exercise Login/Signup Page.
     """
 
-    home = HomePage(page)
-    login = LoginPage(page)
+    # ----------------------------
+    # Login Locators
+    # ----------------------------
 
-    home.open()
+    LOGIN_EMAIL = "input[data-qa='login-email']"
+    LOGIN_PASSWORD = "input[data-qa='login-password']"
+    LOGIN_BUTTON = "button[data-qa='login-button']"
 
-    home.click_login()
+    # ----------------------------
+    # Signup Locators
+    # ----------------------------
 
-    assert "Login" in login.get_title()
+    SIGNUP_NAME = "input[data-qa='signup-name']"
+    SIGNUP_EMAIL = "input[data-qa='signup-email']"
+    SIGNUP_BUTTON = "button[data-qa='signup-button']"
+
+    # ----------------------------
+    # Error Messages
+    # ----------------------------
+
+    LOGIN_ERROR = "form[action='/login'] p"
+
+    def __init__(self, page):
+        super().__init__(page)
+
+    # ----------------------------
+    # Login Methods
+    # ----------------------------
+
+    def enter_login_email(self, email):
+        self.fill(self.LOGIN_EMAIL, email)
+
+    def enter_login_password(self, password):
+        self.fill(self.LOGIN_PASSWORD, password)
+
+    def click_login(self):
+        self.click(self.LOGIN_BUTTON)
+
+    def login(self, email, password):
+        self.enter_login_email(email)
+        self.enter_login_password(password)
+        self.click_login()
+
+    # ----------------------------
+    # Signup Methods
+    # ----------------------------
+
+    def enter_signup_name(self, name):
+        self.fill(self.SIGNUP_NAME, name)
+
+    def enter_signup_email(self, email):
+        self.fill(self.SIGNUP_EMAIL, email)
+
+    def click_signup(self):
+        self.click(self.SIGNUP_BUTTON)
+
+    def signup(self, name, email):
+        self.enter_signup_name(name)
+        self.enter_signup_email(email)
+        self.click_signup()
+
+    # ----------------------------
+    # Validation
+    # ----------------------------
+
+    def get_login_error(self):
+        return self.get_text(self.LOGIN_ERROR)
